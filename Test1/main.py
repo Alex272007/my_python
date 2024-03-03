565f1a02c109da41ea06306223b938ff40e22fb0
Aleksey Kvashnin <aleks272007@gmail.com>
2024-03-03 18:11:05 +0400

    add Python_lessons

---
+++ Test1/main.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+
+one = n % 10
+tens = (n % 100) // 10
+hundreds = (n % 1000) // 100
+
+sum = hundreds + tens + one
+res = sum 
+
+print(res, '(',hundreds, '+', tens, '+', one, ')')
